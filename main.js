067fb649b4f391b09b1b6a16284d0373523fe93e
haobc <18635724353@163.com>
2024-01-13 17:55:42 +0800

    模块拆分

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-import React from './core/React.js'
+import App from './src/App.js'
 import ReactDOM from './core/ReactDOM.js'
-const App = React.createElement("div", { id: "app" }, "Hello mini react!", 'Hi React');
-
-
 ReactDOM.createRoot(document.querySelector("#root")).render(App)
